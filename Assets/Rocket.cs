b240ac6ddd2c402f6052595e43f3bfb0b6edb8a4
Mukit <abdul.mukit64@gmail.com>
2019-11-06 23:16:56 -0600

    added thrust, left-right rotation

---
+++ Assets/Rocket.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    // This function should decide on how to process not perform the action
+    private void ProcessInput()
+    {
+        bool thrust = Input.GetKey(KeyCode.Space);
+        bool turn_left = Input.GetKey(KeyCode.A);
+        bool turn_right = Input.GetKey(KeyCode.D);
+
+        if (thrust)
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (turn_left)
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (turn_right)
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+
+}
