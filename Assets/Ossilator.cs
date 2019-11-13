29d21c1a7b6c6f0787cdcc41edc9e696f3e1a1fa
Abdul-Mukit <abdul.mukit64@gmail.com>
2019-11-12 20:49:36 -0600

    added ossilator script and one ossilating component in level 1

---
+++ Assets/Ossilator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Ossilator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(5f, 0f, 0f);
+    [SerializeField] float period = 2;
+
+    //[Range(0,1)] [SerializeField] float movementFactor;
+    float movementFactor;
+    Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinValue = Mathf.Sin(cycles * tau);
+        movementFactor = rawSinValue/2f + 0.5f;
+
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPos + offset;
+    }
+}
